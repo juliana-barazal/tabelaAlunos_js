a41d2dcab3ca7c0ee530ae2f3f66846c4a882c3d
Juliana Stopassoli Barazal <julianabarazal545@gmail.com>
2024-03-19 15:47:30 -0300

    grade with colors

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function gerarTabela(infos) {
-    var tabela = 
+    let tabela = 
         '<thead>' + '<tr>'
         + '<th>Nome</th>'
         + '<th>Ra</th>'
@@ -11,35 +11,44 @@ function gerarTabela(infos) {
         + '<th>Aprovados</th>'
         + '<th>Reprovados</th>'
         + '</tr>' + '</thead>' + '<tbody>';
-
-    let media = 0;
+    
     let turmaP1 = 0;
     let turmaP2 = 0;
     let aprovados = 0;
     let reprovados = 0;
+    
+
     for (let i in infos) {
         let media = (infos[i].P1 + infos[i].P2) / 2;
         turmaP1 += infos[i].P1 / infos.length;
+        let corP1 = infos[i].P1 >= 6 ? 'blue' :'red';
         turmaP2 += infos[i].P2 / infos.length;
-        tabela += '<tr>'
-            + '<td>' + infos[i].nome + '</td>'
-            + '<td>' + infos[i].ra + '</td>'
-            + '<td>' + infos[i].P1 + '</td>'
-            + '<td>' + infos[i].P2 + '</td>'
-            + '<td>' + media.toFixed(2) + '</td>'
+        let corP2 = infos[i].P2 >= 6 ? 'blue' :'red';
 
+        let corM = '';
         if(media>=6){
             aprovados ++;
+            corM = 'blue';
         }
 
         else{
             reprovados ++;
+            corM ='red';
         }
+
+        tabela += '<tr>'
+            + '<td>' + infos[i].nome + '</td>'
+            + '<td>' + infos[i].ra + '</td>'
+            + '<td style = "color:' + corP1 + '">' + infos[i].P1 + '</td>'
+            + '<td style = "color:' + corP2 + '">' + infos[i].P2 + '</td>'
+            + '<td style = "color:' + corM + '">' + media.toFixed(2) + '</td>'
     }
 
+    let corTP1 = turmaP1 >= 6? 'blue':'red';
+    let corTP2 = turmaP2 >=6? 'blue': 'red';
 
-    tabela += '<td>' + turmaP1.toFixed(2) + '</td>' 
-    + '<td>' + turmaP2.toFixed(2) + '</td>' 
+    tabela += '<td style = "color:'+ corTP1 +'">' + turmaP1.toFixed(2) + '</td>' 
+    + '<td style = "color:' + corTP2 + '">' + turmaP2.toFixed(2) + '</td>' 
     + '<td>' + aprovados + '</td>' 
     + '<td>' + reprovados + '</td>' 
     + '</tr></tbody>'
@@ -47,7 +56,7 @@ function gerarTabela(infos) {
     //cria a tabela no html
     var table = document.createElement('table');
 
-    //conteúdo que está na tabela
+    //aloca let tabela ao conteúdo que está na tabela
     table.innerHTML = tabela;
 
     //adicionar a tabela ao documento
